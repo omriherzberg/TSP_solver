a68ef5f753814e19a9a6a210696ef59b125464a3
omri-herzberg <omri.herzberg@mail.huji.ac.il>
2026-05-02 17:18:12 +0300

    Remove old Fibonacci example

--- fib.c
+++
@@ -1,8 +0,0 @@
-#include "fib.h"
-
-int CFib(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    return CFib(n - 1) + CFib(n - 2);
-}
--- fib.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIB_H
-#define FIB_H
-/**
- * This is a function that calculates the n-th Fibonacci number
- */
-int CFib(int n);
-
-#endif
